6276e3a615ac3c2c7f14499e9b362807a522fc96
Mitchell Wright <mitchellw@digitalwonderlab.com>
2024-06-19 18:26:55 +0100

    Restructured a bit

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,31 @@ use std::time::Duration;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:25565").unwrap();
     println!("listening started, ready to accept");
-    for stream in listener.incoming() {
-        thread::spawn(|| {
-            handle_client(stream.unwrap());
-        });
+    for stream_result in listener.incoming() {
+        match stream_result {
+            Ok(stream) => {
+                thread::spawn(|| {
+                    handle_client(stream);
+                });;
+            },
+            Err(_) => {
+                println!("Failed to accept connection");
+            }
+        }
     }
 }
 
 fn handle_client(stream: TcpStream) {
+
+    // Write to client
     let mut stream = stream;
     stream.write(b"Hello World\r\n").unwrap();
-    let mut buffer = [0; 1024];
+
+    // Give client 5s to write a response to server
     sleep(Duration::from_secs(5));
+
+    // Read UTF8 string from socket
+    let mut buffer = [0; 1024];
     let _ = stream.read(&mut buffer);
     match from_utf8(&buffer) {
         Ok(s) => {
