59fb06b72f15a77f5bc61b2a2def090912ca26ff
Mitchell Wright <mitchellw@digitalwonderlab.com>
2024-06-19 23:02:42 +0100

    Using buffered reader

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use std::io::{Read, Write};
+use std::io::{BufRead, BufReader};
 use std::net::{TcpListener, TcpStream};
-use std::str::from_utf8;
 use std::thread;
-use std::thread::sleep;
-use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:25565").unwrap();
@@ -23,21 +20,17 @@ fn main() {
 }
 
 fn handle_client(stream: TcpStream) {
-
-    // Write to client
-    let mut stream = stream;
-    stream.write(b"Hello World\r\n").unwrap();
-
-    // Give client 5s to write a response to server
-    sleep(Duration::from_secs(5));
-
-    // Read UTF8 string from socket
-    let mut buffer = [0; 1024];
-    let _ = stream.read(&mut buffer);
-    match from_utf8(&buffer) {
-        Ok(s) => {
-            println!("Read: {s}");
+    let mut buffered_reader = BufReader::new(stream);
+    loop {
+        let mut string_buffer = &mut Default::default();
+        match buffered_reader.read_line(string_buffer) {
+            Ok(bytes_read) => {
+                println!("Read {bytes_read} bytes");
+                println!("Value: {string_buffer}");
+            }
+            Err(_) => {
+                panic!("BAD");
+            }
         }
-        Err(_) => {}
     }
 }
